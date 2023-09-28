ed7b41489806dd636d002fd74e612478184e8abc
JostinCabascango <145786732+JostinCabascango@users.noreply.github.com>
2023-09-28 15:16:02 +0200

    Update Tablet.java

--- Tablet.java
+++ Tablet.java
@@ -1,4 +1,4 @@
-public class Tablet extends Dispositiu implements GammaAlta{
+public class Tablet extends Dispositiu implements GammaAlta {
     private int polsades;
 
     public Tablet(String marca, String model, double preuBase, int polsades) {
@@ -14,13 +14,16 @@ public class Tablet extends Dispositiu implements GammaAlta{
         this.polsades = polsades;
     }
 
-  public boolean isGammAlta (){
-    Double preuFinal = super.calcularPreuFinal();
-    if (preuFinal > 900) {
-      return true;
+    @Override
+    public double calcularPreuFinal() {
+        return super.calcularPreuFinal();
     }
-    return false;
-  }
+
+    @Override
+    public boolean isGammaAlta() {
+        return calcularPreuFinal() > 900;
+    }
+
     @Override
     public String toString() {
         return super.toString() + ", nombre de polsades: " + polsades;
