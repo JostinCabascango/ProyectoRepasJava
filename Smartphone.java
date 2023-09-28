871b3f038a51080a074e14b2980429b8f1b03972
JostinCabascango <145786732+JostinCabascango@users.noreply.github.com>
2023-09-28 15:16:22 +0200

    Update Smartphone.java

--- Smartphone.java
+++ Smartphone.java
@@ -1,63 +1,63 @@
-public class Smartphone extends Dispositiu implements GamaAlta{ private String sistema;
-  private boolean accelerometre=false;
-  private boolean gps=true;
+public class Smartphone extends Dispositiu implements GammaAlta {
+    private String sistema;
+    private boolean accelerometre = false;
+    private boolean gps = true;
 
-  public Smartphone(String marca, String model, double preuBase, String sistema, boolean accelerometre, boolean gps) {
-    super(marca, model, preuBase);
-    this.sistema = sistema;
-    this.accelerometre = accelerometre;
-    this.gps = gps;
-  }
-
-  // Getters & Setters
-  public String getSistema() {
-    return sistema;
-  }
-
-  public void setSistema(String sistema) {
-    this.sistema = sistema;
-  }
-
-  public boolean isAccelerometre() {
-    return accelerometre;
-  }
-
-  public void setAccelerometre(boolean accelerometre) {
-    this.accelerometre = accelerometre;
-  }
-
-  public boolean isGps() {
-    return gps;
-  }
-
-  public void setGps(boolean gps) {
-    this.gps = gps;
-  }
-
-  @Override
-  public double calcularPreuFinal() {
-    double preuFinal = super.calcularPreuFinal();
-    if (accelerometre) {
-      // Incrementar el preu final si te accelerometre en 10 %
-      preuFinal = preuFinal * 1.1;
+    public Smartphone(String marca, String model, double preuBase, String sistema, boolean accelerometre, boolean gps) {
+        super(marca, model, preuBase);
+        this.sistema = sistema;
+        this.accelerometre = accelerometre;
+        this.gps = gps;
     }
-    if (gps) {
-      // Incrementar el preu final si te GPS en 5 %
-      preuFinal = preuFinal * 1.05;
+
+    // Getters & Setters
+    public String getSistema() {
+        return sistema;
     }
-    return preuFinal;
-  }
-  public boolean IsGammaAlta() {
-    Double preuFinal = super.calcularPreuFinal();
-    if(preuFinal > 700) {
-      return true;
+
+    public void setSistema(String sistema) {
+        this.sistema = sistema;
     }
-    return false;
-  }
-  @Override
-  public String toString() {
-    return super.toString() + ", Sistema Operatiu: " + sistema + ", acceleròmetre: " + accelerometre + ", GPS: "
-    + gps;
-  }
+
+    public boolean isAccelerometre() {
+        return accelerometre;
+    }
+
+    public void setAccelerometre(boolean accelerometre) {
+        this.accelerometre = accelerometre;
+    }
+
+    public boolean isGps() {
+        return gps;
+    }
+
+    public void setGps(boolean gps) {
+        this.gps = gps;
+    }
+
+    @Override
+    public double calcularPreuFinal() {
+        double preuFinal = super.calcularPreuFinal();
+        if (accelerometre) {
+            // Incrementar el preu final si te accelerometre en 10 %
+            preuFinal = preuFinal * 1.1;
+        }
+        if (gps) {
+            // Incrementar el preu final si te GPS en 5 %
+            preuFinal = preuFinal * 1.05;
+        }
+        return preuFinal;
+    }
+
+    @Override
+    public boolean isGammaAlta() {
+        return calcularPreuFinal() > 700;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Sistema Operatiu: " + sistema + ", acceleròmetre: " + accelerometre + ", GPS: "
+                + gps;
+    }
 
 }
