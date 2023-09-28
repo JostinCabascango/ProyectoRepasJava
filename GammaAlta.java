3c16bd9583d2c797be893c6ec2c38d02b9dd6bba
JunhongITICBCN <146096532+JunhongITIBCN@users.noreply.github.com>
2023-09-28 14:41:29 +0200

    Update GammaAlta.java

--- GammaAlta.java
+++ GammaAlta.java
@@ -1,16 +1,4 @@
 public interface GammaAlta{
-    String dispositiu = dispositivo.getClass();
   public boolean isGammaAlta(float preu){
-    
-    if(dispositivo.getClass().equals("Smartphone")&& preu > 700){
-        return true;
-    }
-    if(dispositivo.getClass().equals("Tablet") && preu > 900){
-        return true;
-    }
-
-    return false;
   }
-
-
 }
